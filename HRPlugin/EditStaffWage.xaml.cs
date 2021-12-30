bc0f7068302178e540f0be1971858fece31abebb
zyue <18642779821@163.com>
2021-12-30 08:39:43 +0800

    更新Hr工资显示

--- HRPlugin/EditStaffWage.xaml.cs
+++ HRPlugin/EditStaffWage.xaml.cs
@@ -66,71 +66,150 @@ namespace HRPlugin
                     models = context.StaffSalary.Where(c => c.StaffId == staffId).OrderBy(c => c.CreateTime).ToList();
             }
 
-            int minValue = 0;//最小值
-            int maxValue = 0;//最大值
-            if (models.Count > 0)
+            //int minValue = 0;//最小值
+            //int maxValue = 0;//最大值
+            //if (models.Count > 0)
+            //{
+            //    minValue = models.Min(c => c.Start).ToString("yyMMdd").AsInt();
+            //    maxValue = models.Where(c => c.IsEnd).Max(c => c.End).ToString("yyMMdd").AsInt();
+            //}
+
+            //Dictionary<int, decimal> priceDic = new Dictionary<int, decimal>();
+            //int number = maxValue - minValue;
+            //for (int i = 0; i < number; i++)
+            //{
+            //    priceDic.Add(minValue + i, 0);
+            //}
+
+            //for (int i = 0; i < models.Count; i++)
+            //{
+            //    var _item = models[i];
+
+            //    int itemStart = _item.Start.ToString("yyMMdd").AsInt();
+            //    int itemEnd = _item.IsEnd ? _item.End.ToString("yyMMdd").AsInt() : 0;
+            //    decimal itemPrice = _item.Price;
+
+            //    if (itemEnd == 0)
+            //    {
+            //        for (int j = itemStart; j <= maxValue; j++)
+            //        {
+            //            if (priceDic.ContainsKey(j))
+            //            {
+            //                priceDic[j] = itemPrice;
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        for (int j = itemStart; j <= itemEnd; j++)
+            //        {
+            //            if (priceDic.ContainsKey(j))
+            //            {
+            //                priceDic[j] = itemPrice;
+            //            }
+            //        }
+            //    }
+            //}
+            //if (priceDic.Keys.Count == 0)
+            //    lblWageNow.Content = 0;
+            //else
+            //    lblWageNow.Content = priceDic[DateTime.Now.ToString("yyMMdd").AsInt()];
+            //decimal currPrice = 0;
+            //for (int i = minValue; i < maxValue; i++)
+            //{
+            //    if (priceDic[i] != currPrice)
+            //    {
+            //        string _s = i.ToString();
+            //        tlWage.Items.Add(new TimelineItem()
+            //        {
+            //            Content = priceDic[i],
+            //            Header = $"20{_s.Substring(0, 2)}年{_s.Substring(2, 2)}月{_s.Substring(4, 2)}",
+            //            Height = 60,
+            //        });
+            //        currPrice = priceDic[i];
+            //    }
+            //}
+
+            WAGELIST.Clear();
+
+            SetWage(staff.CreateTime, DateTime.MaxValue, 0);
+
+            foreach (var item in models)
             {
-                minValue = models.Min(c => c.Start).ToString("yyMMdd").AsInt();
-                maxValue = models.Where(c => c.IsEnd).Max(c => c.End).ToString("yyMMdd").AsInt();
+                DateTime end = item.End;
+                if (!item.IsEnd) end = DateTime.MaxValue;
+                SetWage(item.Start, end, item.Price);
             }
 
-            Dictionary<int, decimal> priceDic = new Dictionary<int, decimal>();
-            int number = maxValue - minValue;
-            for (int i = 0; i < number; i++)
+            DateTime maxDate = DateTime.MaxValue.MinDate();
+            if (WAGELIST.Any(c => c.Start == maxDate)) 
+                WAGELIST.Remove(WAGELIST.First(c => c.Start == maxDate));
+
+            WAGELIST = WAGELIST.OrderBy(c => c.Start).ToList();
+
+            foreach (var item in WAGELIST)
             {
-                priceDic.Add(minValue + i, 0);
+                tlWage.Items.Add(new TimelineItem()
+                {
+                    Content = item.Price,
+                    Header = item.Start.ToString("yy年MM月dd日"),
+                    Height = 60,
+                });
             }
 
-            for (int i = 0; i < models.Count; i++)
+            //当前工资额
+            lblWageNow.Content = 0;
+            if (WAGELIST.Any(c => c.Start < DateTime.Now)) 
             {
-                var _item = models[i];
-
-                int itemStart = _item.Start.ToString("yyMMdd").AsInt();
-                int itemEnd = _item.IsEnd ? _item.End.ToString("yyMMdd").AsInt() : 0;
-                decimal itemPrice = _item.Price;
-
-                if (itemEnd == 0)
-                {
-                    for (int j = itemStart; j <= maxValue; j++)
-                    {
-                        if (priceDic.ContainsKey(j))
-                        {
-                            priceDic[j] = itemPrice;
-                        }
-                    }
-                }
-                else
-                {
-                    for (int j = itemStart; j <= itemEnd; j++)
-                    {
-                        if (priceDic.ContainsKey(j))
-                        {
-                            priceDic[j] = itemPrice;
-                        }
-                    }
-                }
-            }
-            if (priceDic.Keys.Count == 0)
-                lblWageNow.Content = 0;
-            else
-                lblWageNow.Content = priceDic[DateTime.Now.ToString("yyMMdd").AsInt()];
-            decimal currPrice = 0;
-            for (int i = minValue; i < maxValue; i++)
-            {
-                if (priceDic[i] != currPrice)
-                {
-                    string _s = i.ToString();
-                    tlWage.Items.Add(new TimelineItem()
-                    {
-                        Content = priceDic[i],
-                        Header = $"20{_s.Substring(0, 2)}年{_s.Substring(2, 2)}月{_s.Substring(4, 2)}",
-                        Height = 60,
-                    });
-                    currPrice = priceDic[i];
-                }
+                lblWageNow.Content = WAGELIST.Last(c => c.Start < DateTime.Now).Price;
             }
 
             tlWage.IsEnabled = true;
+        }
+
+        class WageModel
+        {
+            public DateTime Start { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        List<WageModel> WAGELIST = new List<WageModel>();
+
+        private void SetWage(DateTime _start, DateTime _end, decimal _price)
+        {
+            _start = _start.MinDate();
+            _end = _end.MinDate();
+
+            if (WAGELIST.Count == 0)
+            {
+                WAGELIST.Add(new WageModel() { Start = _start, Price = _price });
+                WAGELIST.Add(new WageModel() { Start = _end, Price = _price });
+                return;
+            }
+
+            WageModel start = new WageModel();
+            WageModel end = new WageModel();
+
+            start.Start = _start;
+            start.Price = _price;
+
+            if (WAGELIST.Any(c => c.Start == _start))
+            {
+                WAGELIST.Remove(WAGELIST.First(c => c.Start == _start));
+            }
+
+            end.Start = _end;
+            end.Price = WAGELIST.Any(c => c.Start < _end) ? WAGELIST.First(c => c.Start < _end).Price : _price;
+
+            if (WAGELIST.Any(c => c.Start == _end))
+            {
+                WAGELIST.Remove(WAGELIST.First(c => c.Start == _end));
+            }
+
+            WAGELIST.RemoveAll(c => c.Start >= _start && c.Start <= _end);
+
+            WAGELIST.Add(start);
+            WAGELIST.Add(end);
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
